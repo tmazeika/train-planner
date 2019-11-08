3f8c5fa75fc4202fe8647f83edff28d3a94949ec
T.J. Mazeika <mazeika.tj@gmail.com>
2019-11-08 02:05:47 -0500

    Clean up HTTP request

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"compress/gzip"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
@@ -54,8 +53,6 @@ func getHtml(when time.Time, from, to string) io.ReadCloser {
 		"WAS": "Washington, DC",
 	}
 	date := when.Format("01/02/2006")
-	now := time.Now()
-	year, month, day := now.Date()
 
 	if _, ok := knownStations[from]; !ok {
 		panic("invalid from station: " + from)
@@ -65,78 +62,32 @@ func getHtml(when time.Time, from, to string) io.ReadCloser {
 	}
 
 	form := url.Values{}
-	form.Add("wdf_TripType", "OneWay")
 	form.Add("wdf_origin", from)
 	form.Add("wdf_origin", knownStations[from])
-	form.Add("statesType", "AL")
-	form.Add("statesType", "AB")
-	form.Add("countryType", "US")
 	form.Add("wdf_destination", to)
 	form.Add("wdf_destination", knownStations[to])
-	form.Add("statesType", "AL")
-	form.Add("statesType", "AB")
-	form.Add("countryType", "US")
 	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/journeyRequirements[1]/departDate.usdate", date)
-	form.Add("_ff_time_of_day", "AllDay")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/@adultTravellers", "1")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/numberOfTravellers[@key='Senior']", "0")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/@youthTravellers", "0")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/numberOfTravellers[@key='Child']", "0")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/numberOfTravellers[@key='Infant']", "0")
 	form.Add("xwdf_person_type1", "/sessionWorkflow/productWorkflow[@product=\"Rail\"]/tripRequirements/allJourneyRequirements/person[1]/personType")
 	form.Add("wdf_person_type1", "Adult")
 	form.Add("_handler=amtrak.presentation.handler.request.rail.farefamilies.AmtrakRailFareFamiliesSearchRequestHandler/_xpath=/sessionWorkflow/productWorkflow[@product='Rail'].x", "62")
-	form.Add("xwdf_promoCode", "/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/couponRequirements/coupon/code")
-	form.Add("wdfdate1", fmt.Sprintf("%d-%d-%d", year, int(month)-1, day))
-	form.Add("searchFlag", "true")
-	form.Add("xwdf_promoCode", "/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/couponRequirements/coupon/code")
-	form.Add("wdf_promoCode", "")
-	form.Add("wdf_BookType_homepage", "")
-	form.Add("radio", "on")
 	form.Add("xwdf_origin", "/sessionWorkflow/productWorkflow[@product='Rail']/travelSelection/journeySelection[1]/departLocation/search")
 	form.Add("xwdf_destination", "/sessionWorkflow/productWorkflow[@product='Rail']/travelSelection/journeySelection[1]/arriveLocation/search")
-	form.Add("xwdf_BookType_homepage", "/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/@booktype_homepage")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/@bookpath", "farefamilies")
-	form.Add("requestor", "amtrak.presentation.handler.page.rail.AmtrakRailFareFinderPageHandler")
-	form.Add("xwdf_TripType", "/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/tripType")
-	form.Add("/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/numberOfTravellers[@key='Adult']", "1")
 
 	req, err := http.NewRequest(http.MethodPost, "https://tickets.amtrak.com/itd/amtrak", strings.NewReader(form.Encode()))
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("Host", "tickets.amtrak.com")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Pragma", "no-cache")
-	req.Header.Set("Cache-Control", "no-cache")
-	req.Header.Set("DNT", "1")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36")
-	req.Header.Set("Sec-Fetch-User", "?1")
-	req.Header.Set("Origin", "https://tickets.amtrak.com")
+	req.Header.Set("Accept", "text/html")
+	req.Header.Set("Accept-Language", "en-US, en;q=0.9")
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-Mode", "navigate")
-	req.Header.Set("Referer", "https://www.amtrak.com/home.html")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
 	req.Header.Set("Cookie", "ADRUM=s=1573183464194&r=https%3A%2F%2Ftickets.amtrak.com%2Fitd%2Famtrak%3F0")
+	req.Header.Set("Referer", "https://www.amtrak.com/home.html")
+	req.Header.Set("User-Agent", "train-planner")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
-
-	var reader io.ReadCloser
-	switch resp.Header.Get("Content-Encoding") {
-	case "gzip":
-		if reader, err = gzip.NewReader(resp.Body); err != nil {
-			panic(err)
-		}
-	default:
-		reader = resp.Body
-	}
-	return reader
+	return resp.Body
 }
